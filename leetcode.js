ff57cac8252f4cf95a5a9ad0683605a60e0962e6
Eitan Elberg <eitan.elberg@consumrz.com>
2023-08-01 18:06:36 +0300

    leetcode

--- leetcode.js
+++ leetcode.js
@@ -6,7 +6,7 @@
 // https://leetcode.com/problems/maximum-subarray/description/
 
 // var maxSubArray = function(nums) {
-//     let maxSum = nums[0];
+//     let maxSum = nums[0];  
 
 //     for (let i = 1; i < nums.length; i++) {
 //         nums[i] = Math.max(0, nums[i - 1]) + nums[i];
