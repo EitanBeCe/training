9a6e933d483c0b394301a4b7efea7c833f8a46b5
Eitan Elberg <eitan.elberg@consumrz.com>
2023-08-09 12:37:39 +0300

    leetcode

--- leetcode.js
+++ leetcode.js
@@ -2,9 +2,48 @@
 
 
 
+// 4
+// https://leetcode.com/problems/move-zeroes/description/
+// const nums = [0,1,0,3,12]
+// var moveZeroes = function(nums) {
+// // var moveZeroes = function(nums: number[]) {
+
+//     // 2 changing arr itself
+//     let size = nums.length;
+
+//     for (let i = 0; i < size; i++) {
+//         if (nums[i] === 0) {
+//             nums.splice(i, 1);
+//             nums.push(0);
+//             i--;
+//             size--;
+//         }
+//     }
+
+//     return nums
+    
+//     // 1 with returning in filter
+//     let countZeros = 0
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] === 0) {
+//             countZeros++
+//         }
+        
+//     }
+
+//     nums = nums.filter(num => num != 0)
+
+//     for (let i = 0; i < countZeros; i++) {
+//         nums.push(0)
+//     }
+
+//     console.log(nums);
+// };
+// moveZeroes(nums)
+
 // 3
 // https://leetcode.com/problems/maximum-subarray/description/
-
 // var maxSubArray = function(nums) {
 //     let maxSum = nums[0];  
 
